5c9fc64b0db40bb3893822b40dbc022f5533e8d8
JonasMoreira56 <jonasmoreira076@gmail.com>
2025-05-14 18:02:15 -0300

    raw: manutenção email

--- src/dirigible_control/setup.py
+++ src/dirigible_control/setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ros2',
-    maintainer_email='jonas.barbosa@ufam.edu.br',
+    maintainer_email='ros2@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
