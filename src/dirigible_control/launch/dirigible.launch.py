32f258d49b6a1488b4e3ba675f50f6aa9fbad42f
JonasMoreira56 <jonasmoreira076@gmail.com>
2025-06-10 00:09:00 -0300

    refactor: refatoracao no arquivo de lancamento para abrir automaticamente os modelos

--- src/dirigible_control/launch/dirigible.launch.py
+++ src/dirigible_control/launch/dirigible.launch.py
@@ -1,37 +1,59 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 import os
 from ament_index_python.packages import get_package_share_directory
 
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 def generate_launch_description():
-    # Obter o diretório do pacote
+     
     package_dir = get_package_share_directory('dirigible_control')
+    world_path = os.path.join(package_dir, 'worlds', 'empty.world')
+    models_path = os.path.join(package_dir, 'models')
 
-    # Construir o caminho para o arquivo world
-    # AGORA APONTA PARA O ARQUIVO .world NA PASTA worlds
-    world_path = os.path.join(package_dir, 'worlds', 'world_airship.world')
-
-    # Verificar se o arquivo de MUNDO existe
     if not os.path.exists(world_path):
         raise FileNotFoundError(f"Arquivo de mundo não encontrado: {world_path}")
 
-    # Imprime o caminho do mundo para depuração
-    print(f"World path: {world_path}")
+    print(f"World path (Caminho Mundo): {world_path}")
+    print(f"Models path (Caminho model): {models_path}")
 
     return LaunchDescription([
+        # Adiciona o diretório de modelos ao GAZEBO_MODEL_PATH
+        SetEnvironmentVariable( 
+            name='GAZEBO_MODEL_PATH',
+            value=models_path
+        ),
 
-        # Iniciar o Gazebo com o ARQUIVO DE MUNDO
         ExecuteProcess(
-            # AGORA PASSA O CAMINHO DO MUNDO PARA O GAZEBO
-            cmd=['gazebo', '--verbose', world_path], # Adicionado --verbose para mais detalhes
+            cmd=['gazebo', '--verbose', world_path],
             output='screen'
         ),
 
-        # Iniciar o nó ROS 2 (sem alterações aqui)
         Node(
             package='dirigible_control',
             executable='dirigible_controller',
             output='screen'
         )
     ])
+
+# def generate_launch_description():
+#     # Caminho para o diretório de instalação do Gazebo
+#     gazebo_ros_dir = get_package_share_directory('gazebo_ros')
+    
+#     # Caminho para o seu arquivo de mundo
+#     my_world_path = os.path.join(get_package_share_directory('dirigible_control'), 'worlds', 'empty.world')
+    
+#     # Iniciar o Gazebo
+#     gazebo_launch = IncludeLaunchDescription(
+#         PythonLaunchDescriptionSource(
+#             os.path.join(gazebo_ros_dir, 'launch', 'gazebo.launch.py')
+#         ),
+#         launch_arguments={'world': my_world_path}.items()
+#     )
+
+#     return LaunchDescription([
+#         gazebo_launch,
+#         # Adicione outros nós ROS aqui, se necessário
+#     ])
